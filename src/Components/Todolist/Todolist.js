32f77e24f1a8435ba50b53a28be8d8bea900eb27
MohamadTdn <mohamadmt1579@gmail.com>
2024-06-06 01:18:42 +0330

    add todos in list

--- src/Components/Todolist/Todolist.js
+++ src/Components/Todolist/Todolist.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Todolist.css'
+import { MdDone } from "react-icons/md";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 export default function Todolist() {
   return (
@@ -8,9 +10,33 @@ export default function Todolist() {
         <input type="text" className='Todo-input' placeholder='Enter the Todo' />
         <input type="submit" className='submit-btn' value="Add" />
       </form>
-
+      <select className='Filter'>
+        <option value="All">All</option>
+        <option value="Complete">Complete</option>
+        <option value="Incomplete">Incomplete</option>
+      </select>
       <ul className='Todos'>  
-        
+        <li className='Todo'>
+          <span className='Todo-title'>Test todo</span>
+          <div>
+            <button className='Done-btn'><MdDone/></button>
+            <button className='Delete-btn'><FaRegTrashAlt/></button>
+          </div>
+        </li>
+        <li className='Todo'>
+          <span className='Todo-title'>Test todo</span>
+          <div>
+            <button className='Done-btn'><MdDone/></button>
+            <button className='Delete-btn'><FaRegTrashAlt/></button>
+          </div>
+        </li>
+        <li className='Todo'>
+          <span className='Todo-title'>Test todo</span>
+          <div>
+            <button className='Done-btn'><MdDone/></button>
+            <button className='Delete-btn'><FaRegTrashAlt/></button>
+          </div>
+        </li>
       </ul>
     </div>
   )
